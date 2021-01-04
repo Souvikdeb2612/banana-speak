b4bb4e5bdbea769cd55531b3eaef32801c82eceb
Souvikdeb2612 <souvikdeb26@gmail.com>
2021-01-05 01:17:52 +0530

    styling

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#text-input");
+var txtOutput = document.querySelector("#output")
+
+// var serverURL ="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL ="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslatedURL(text){
+    return serverURL + "?" + "text="+ text
+}
+
+function errorHandler(error){
+    console.log("error occured", error)
+    alert("Something wrong with the server. Try again after sometime")
+}
+
 
 console.log(txtInput)
 
 function clickHandler(){
-    console.log("clicked");
-    console.log("input", txtInput.value)
+    var inputText = txtInput.value;
+
+    fetch(getTranslatedURL(inputText))
+        .then(response => response.json())
+        .then(json=> {
+            var translatedText = json.contents.translated;
+            txtOutput.innerText = translatedText;
+        })
+        .catch(errorHandler)
 };
+
+
+
 
 btnTranslate.addEventListener("click",clickHandler)
